0e1c5ca1c9095bf3b5e46390b0878c3a22a3fff7
WoonSung Baek <woonsungbaek@WoonSungui-MacBook-Pro.local>
2016-05-16 13:51:11 +0900

    [1.12] bugfix QRcode parser

--- app/src/main/java/com/b2soft/lottogo/LottoQRParser.java
+++ app/src/main/java/com/b2soft/lottogo/LottoQRParser.java
@@ -4,6 +4,7 @@ import com.b2soft.lottogo.model.my.DrwtNos;
 import com.b2soft.lottogo.model.my.ResultHistoryNo;
 import com.b2soft.lottogo.utils.BBLogger;
 
+import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,7 +19,7 @@ public class LottoQRParser {
     ResultHistoryNo resultHistoryNo;
 
     public LottoQRParser(String urlValue) {
-        pasing(urlValue);
+        parsing(urlValue);
     }
 
     public int getDrwNo() {
@@ -29,7 +30,7 @@ public class LottoQRParser {
         return resultHistoryNo;
     }
 
-    protected void pasing(String url) {
+    protected void parsing(String url) {
         Pattern urlPattern = Pattern.compile("^(https?):\\/\\/([^:\\/\\s]+)(:([^\\/]*))?((\\/[^\\s/\\/]+)*)?\\/([^#\\s\\?]*)(\\?([^#\\s]*))?(#(\\w*))?$");
         Matcher mc = urlPattern.matcher(url);
         if (mc.find()) {
@@ -46,14 +47,27 @@ public class LottoQRParser {
             logger.debug("key: " + temp[1]);
             resultHistoryNo.setKey(temp[1]);
 
-            String[] value = temp[1].split("m");
-            logger.debug("drwtNo: " + Integer.parseInt(value[0]));
-            resultHistoryNo.setDrwNo(Integer.parseInt(value[0]));
+            /*
+                m : menual
+                q : auto
+                ex) temp[1] : 0702q101924263545q142630353745q031012163741q081022252740q0306363839451703707392
+                value[0] : drwtNo
+                value[1] : numbers 1
+                value[2] : numbers 2
+                value[3] : numbers 3
+                value[4] : numbers 4
+                value[5] : numbers 5
+              */
 
-            String[] numbers = value[1].split("q");
+//            StringTokenizer st = new StringTokenizer(temp[1], "m|q");
+            String value[] = temp[1].split("m|q");
+            int drwtNo = Integer.parseInt(value[0]);
+            logger.debug("drwtNo: " + drwtNo);
+            resultHistoryNo.setDrwNo(drwtNo);
+
             resultHistoryNo.setDrwtNoList(new RealmList<DrwtNos>());
-            for (int i = 0; i < numbers.length; i++) {
-                String number = numbers[i].substring(0, 12);
+            for (int i = 1; i < value.length; i++) {
+                String number = value[i].substring(0, 12);
                 logger.debug("numbers: " + number);
                 DrwtNos drwtNos = new DrwtNos();
                 int k = 0;
